78c4bdcdb527ccb5c3d50ddbdc6fb775eaa24672
Your Name <josieacarrillo@gmail.com>
2019-05-27 20:36:01 -0700

    handler input

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const Alexa = require('ask-sdk-core');
+
 const HelloHandler = {
   canHandle(handlerInput) {
     return handlerInput.requestEnvelope.request.type === 'IntentRequest'
       && handlerInput.requestEnvelope.request.intent.name === 'HelloIntent';
   },
+  handle(handlerInput) {
+   const speakOutput = 'Hello Codecademy';
+
+   return handlerInput.responseBuilder
+      .speak(speakOutput)
+      .getResponse();
+	}
 };
-testing, 1,2.
