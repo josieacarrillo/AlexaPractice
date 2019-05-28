149f6c465b1402eca4277fa7864bc70cfbddff5f
Your Name <josieacarrillo@gmail.com>
2019-05-27 20:28:43 -0700

    push

---
+++ index.js
@@ -0,0 +1,7 @@
+const Alexa = require('ask-sdk-core');
+const HelloHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'HelloIntent';
+  },
+};
